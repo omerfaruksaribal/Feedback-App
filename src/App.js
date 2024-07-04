0b045a8f2b2244d04b45f685aada8e49435780e1
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2024-07-04 10:32:43 +0300

    Project is done Version: 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React  from "react"
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from "./pages/AboutPage"
+import AboutIconLink from "./components/AboutIconLink"
+
 
 
 function App() {
@@ -24,14 +28,25 @@ function App() {
     }
 
     return (
-    <>
+    <Router>
         <Header />
         <div className="container">
-            <FeedbackForm handleAdd={addFeedback} />
-            <FeedbackStats feedback = {feedback} />
-            <FeedbackList feedback = {feedback} handleDelete={ deleteFeedback } />
+            <Routes>
+                <Route exact path="/" element={
+                    <>
+                        <FeedbackForm handleAdd={addFeedback} />
+                        <FeedbackStats feedback = {feedback} />
+                        <FeedbackList feedback = {feedback} handleDelete={ deleteFeedback } />
+                    </>
+                }>
+                    
+                </Route>
+                <Route path="/about" element={<AboutPage />}/>
+            </Routes>
+
+            <AboutIconLink />
         </div>
-     </>   
+     </Router>   
     )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 
 function Header({ text, bgColor, textColor }) {
 
@@ -11,7 +12,9 @@ function Header({ text, bgColor, textColor }) {
     return (
         <header style={headerStyles}>
             <div className="container">
-                <h2>{text}</h2>
+                <h2>
+                    <Link to="/" >{text}</Link>
+                </h2>
             </div>
         </header>
   )
@@ -21,6 +24,7 @@ Header.defaultProps = {
     text: 'Feedback UI',
     bgColor: 'rgba(0,0,0,0.4)',
     textColor: '#ff6a95',
+
 }
 
 Header.propTypes = {
